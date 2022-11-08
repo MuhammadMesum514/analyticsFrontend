3b75a8dcfb4073ba1458c55b4f08c5029794b53a
Muhammad Mesum <mesumraza09@gmail.com>
2022-11-08 12:36:57 +0500

    code updated for analytics

---
+++ README.md
@@ -0,0 +1,6 @@
+MELEGA FINANCE ANALYTICS
+
+run command:
+`yarn` // install dependencies
+`yarn start` start Project
+`yarn Build` build Project for Production
---
+++ src/apollo/client.js
@@ -0,0 +1,44 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    // uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2',
+    uri: 'https://api.thegraph.com/subgraphs/name/muhammadmesum514/melegafinance',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/index-node/graphql',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/muhammadmesum514/melegafinancenew',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    // uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+    uri: 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks',
+  }),
+  cache: new InMemoryCache(),
+})
